73ccbfafc53c8e6c989102b63420c29073ea3c2c
rothehuman <99630218+rothehuman@users.noreply.github.com>
2023-03-07 10:18:21 -0300

    JS task2 v1.0

---
+++ assets/js/pastEvents.js
@@ -0,0 +1,31 @@
+import data from "./amazing.js"
+
+//Dado un arreglo de events y una fecha, retorna un ARREGLO con los eventos anteriores a la fecha pasada
+function pastEvents(events, datee){
+    let cardsE = ''
+
+    for(let event of events){
+        if (event.date < datee) {
+            cardsE += `
+                <div class="card m-3" style="width: 18rem; height: 25rem;">
+                <img src="${event.image}" class="card-img-top p-2" alt="...">
+                <div class="card-body text-center card-body-header my-0 py-0">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                </div>
+                <div class="card-body d-flex flex-row justify-content-between">
+                    <p>Price $${event.price}</p>
+                    <a href="../../pages/details.html" class="btn btn-light btn-outline-dark">view more</a>
+                </div>
+                </div>
+            `
+        }
+    }
+    return cardsE;
+}
+
+const cards = document.querySelector('#eventCards');
+
+let tarjetas = pastEvents(data.events, data.currentDate);
+
+cards.innerHTML = tarjetas
